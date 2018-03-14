a8c7a6ef9cbbddf39c0c507ca8bce615a4ccfcbf
Stephanie Shea <stephcshea@gmail.com>
2018-03-14 16:51:48 +0000

    added news API

---
+++ public/angular-app/news.js
@@ -0,0 +1,62 @@
+/*global $*/
+/*global APIKEY $*/
+$(document).ready(function() {
+	$.ajax({
+		method: "GET",
+		url: "https://newsapi.org/v2/sources",
+		data: {
+			category: "business",
+			country: "us",
+			language: "en",
+			apiKey: "13b97311a2014874a45db72ad3c7ca88"
+		},
+		success: function(data) {
+			if (data.status === "ok") {
+				console.log(data)
+				for (var i = 0; i < data.sources.length; i++) {
+					var source = document.createElement("OPTION");
+					source.setAttribute("value", data.sources[i].id);
+					source.innerHTML = data.sources[i].name;
+					document.getElementById("selection").appendChild(source);
+				}
+			}
+		}
+	});
+	
+	
+	$("#source").submit(function(event) {
+		event.preventDefault();
+		document.getElementById("heading").innerHTML = "";
+		document.getElementById("heading").innerHTML+=("Top Business Headlines");
+		document.getElementById("headlines").innerHTML = "";
+		$.ajax({
+			method: "GET",
+			url: "https://newsapi.org/v2/top-headlines",
+			data: {
+				sources: document.getElementById("selection").value, apiKey: "13b97311a2014874a45db72ad3c7ca88"
+			},
+			success: function(data) {
+				console.log(data);
+				if (data.status === "ok") {
+					for (var i = 0; i < data.articles.length; i++) {
+						console.log(data.articles[i].title);
+						var headline = document.createElement("li");
+						headline.innerHTML = data.articles[i].title;
+						var description = document.createElement("p");
+						description.innerHTML = data.articles[i].description;
+						var url = document.createElement('a');
+						url.setAttribute('href', data.articles[i].url);
+						url.innerHTML = data.articles[i].url;
+						// var urlToImage = document.createElement('IMG');					
+						// urlToImage.setAttribute('src', data.articles[i].urlToImage);
+						// urlToImage.innerHTML = data.articles[i].urlToImage;
+						// document.getElementById("headlines").appendChild(urlToImage);
+						document.getElementById("headlines").appendChild(headline);
+						document.getElementById("headlines").appendChild(description);
+				        document.getElementById("headlines").appendChild(url);
+					}
+				}
+			}
+		});
+	});
+});	
